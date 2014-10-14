886fd69e2c438da239844011aa17c5608f9bed12
Anastasya Shirokovska <anastasya.shir@gmail.com>
2014-10-14 23:55:53 +0300

    sortingStation - brackets

--- src/main/java/com.zurmansor.calculator.main/Main.java
+++ src/main/java/com.zurmansor.calculator.main/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
 
 public class Main {
@@ -6,28 +7,39 @@ public class Main {
 
         String result = "";
         Deque<Character> stack = new ArrayDeque<Character>();
-
+        ArrayList<Character> numbers = new ArrayList<Character>();
+// TODO: большие числа
         for (int i = 0; i < string.length(); i++) {
+            // цифры в строку
             if (Character.isDigit(string.charAt(i))) {
                 result += string.charAt(i);
-            } else {
+
+            } else if (string.charAt(i)=='(') {
+                    stack.push(string.charAt(i));
+            }else if (string.charAt(i) == ')') {
+
+// TODO:если мы не находим (
+// TODO: оптимизировать вайл
+                while (!stack.peek().equals('(')) {
+                    result += stack.pop();
+                    }
+                stack.pop();
+            }
+            //операции в стек
+            else {
                 stack.push(string.charAt(i));
             }
         }
         while (!stack.isEmpty()) {
-
             result += stack.pop();
         }
-
-
 
         return result;
     }
 
     public static void main (String[] args) {
 
-        System.out.println(convert("3+5-8"));
+        System.out.println(convert("(1+2)*4"));
     }
-
 
 }
