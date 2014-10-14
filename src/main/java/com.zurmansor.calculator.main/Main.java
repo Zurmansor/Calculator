d48c9cf34d902b9da87da1b136601950020996c4
Anastasya Shirokovska <anastasya.shir@gmail.com>
2014-10-15 00:54:46 +0300

    sortingStation - Sorting on equal priorities

--- src/main/java/com.zurmansor.calculator.main/Main.java
+++ src/main/java/com.zurmansor.calculator.main/Main.java
@@ -3,31 +3,40 @@ import java.util.ArrayList;
 import java.util.Deque;
 
 public class Main {
-    private static String convert(String string) {
+    private static String sortingStation(String string) {
 
         String result = "";
+        char currentCharacter;
         Deque<Character> stack = new ArrayDeque<Character>();
         ArrayList<Character> numbers = new ArrayList<Character>();
-// TODO: большие числа
+        // TODO: большие числа
+
         for (int i = 0; i < string.length(); i++) {
+            currentCharacter = string.charAt(i);
             // цифры в строку
-            if (Character.isDigit(string.charAt(i))) {
-                result += string.charAt(i);
-
-            } else if (string.charAt(i)=='(') {
-                    stack.push(string.charAt(i));
-            }else if (string.charAt(i) == ')') {
-
-// TODO:если мы не находим (
-// TODO: оптимизировать вайл
+            if (Character.isDigit(currentCharacter)) {
+                result += currentCharacter;
+            } else if (currentCharacter=='(') {
+                stack.push(currentCharacter);
+            }else if (currentCharacter == ')') {
+                // TODO:если мы не находим '('
+                // TODO: оптимизировать вайл
                 while (!stack.peek().equals('(')) {
                     result += stack.pop();
-                    }
+                }
                 stack.pop();
             }
             //операции в стек
             else {
-                stack.push(string.charAt(i));
+                // если приоритет операции выше последней операции в стеке, то она идет напрямую в result
+                // в другом случае она идет в стек, а последняя стековая идет в result
+// TODO: только если используются операции + или -
+
+//                System.out.println();
+                if (!stack.isEmpty() && !stack.peek().equals('(')) {
+                    result += stack.pop();
+                }
+                stack.push(currentCharacter);
             }
         }
         while (!stack.isEmpty()) {
@@ -38,8 +47,10 @@ public class Main {
     }
 
     public static void main (String[] args) {
+        String expr1 = "1+2-(4+6-7)";
+        String expr2 = "1+2-4+6-7+1";
 
-        System.out.println(convert("(1+2)*4"));
+        System.out.println(sortingStation(expr1));
     }
 
 }
